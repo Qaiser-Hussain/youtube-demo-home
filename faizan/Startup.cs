8a3edff23dd31bad5cbe35b9f267e68e3c7b8ae3
Qaiser-Hussain <qaiserui11@gmail.com>
2017-04-15 12:10:00 +0500

    initial commit

---
+++ faizan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(faizan.Startup))]
+namespace faizan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
